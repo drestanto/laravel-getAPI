a2b721ad1803ad35f2cb3e79925504b7b6daeeeb
Dyas <dyas@live.com>
2017-06-06 11:20:04 +0700

    add route and view

---
+++ resources/views/forex_api.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title> ForEx </title>
+</head>
+<body>
+	<h1> Foreign Exchange </h1>
+	JSON String : <br>
+	blah
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/forex', function () {
+	return view('forex_api');
+});
